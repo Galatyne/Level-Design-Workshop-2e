376e426e420e6f3e80bf32ee0a6c692290927854
Fidy <fidygalatyne@proton.me>
2025-12-12 17:43:55 +0100

    Beta 2

--- Assets/Scripts/tourelle.cs
+++ Assets/Scripts/tourelle.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using JU.CharacterSystem.AI;
 
 public class tourelle : MonoBehaviour
 {
+    public JU_AI_PatrolCharacter patrol;
+    
     private void OnDrawGizmos()
     {
         Gizmos.color = new Color(1f, 0f, 0f, 1);
         Gizmos.color = Color.white;
-        Gizmos.DrawWireSphere(transform.position, 40);
+        Gizmos.DrawWireSphere(transform.position, patrol.FieldOfView.Distance);
     }
 }
